1320872b85bd410adf44c60cda86aa335dab7dd5
andrey <andreygabrielben@gmail.com>
2025-09-03 19:47:35 -0300

    teste da função  sortear

---
+++ app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let QuantidadeASortear = document.getElementById("quantidade").value;
+    let min = document.getElementById("de").value;
+    let max = document.getElementById("ate").value;
+
+    console.log(`quantidade a sortear: ${QuantidadeASortear}`);
+    console.log(`min: ${min}`);
+    console.log(`max: ${max}`); 
+};
