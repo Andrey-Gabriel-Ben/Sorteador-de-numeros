970891d3450841851db51fa5cacb20f609ae158d
andrey <andreygabrielben@gmail.com>
2025-09-15 19:34:05 -0300

    Adicionar o diminuidor de loop, porém incompleto

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 function sortear() {
-    let QuantidadeASortear = document.getElementById("quantidade").value;
-    let min = document.getElementById("de").value;
-    let max = document.getElementById("ate").value;
+    let QuantidadeASortear = parseInt(document.getElementById("quantidade").value);
+    let min = parseInt(document.getElementById("de").value);
+    let max = parseInt(document.getElementById("ate").value);
     let QuantidadeRestante = QuantidadeASortear;
     if (QuantidadeRestante > 1) {
-        
+        QuantidadeRestante--;
     }
 };
+
