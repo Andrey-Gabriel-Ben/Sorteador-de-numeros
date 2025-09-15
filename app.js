4c33be6068c8479d786bd8d1243a85fcf6a5dce2
andrey <andreygabrielben@gmail.com>
2025-09-15 20:11:00 -0300

    finalização da função de geração de números aleatórios utilizando os valores de min e max declarados no front do site

--- app.js
+++ app.js
@@ -2,13 +2,18 @@ function sortear() {
     let QuantidadeASortear = parseInt(document.getElementById("quantidade").value);
     let de = parseInt(document.getElementById("de").value);
     let ate  = parseInt(document.getElementById("ate").value);
-    let numerosSorteados = obterNumeroAleatorio(de, ate);
-    let QuantidadeRestante = QuantidadeASortear;
-    if (QuantidadeRestante > 1) {
-        QuantidadeRestante--;
-    }
-};
+    let numerosSorteados = [];
+    let valorsorteado;
 
-function obterNumeroAleatorio(min, max) {
+    for (let i=0; i < QuantidadeASortear; i++) {
+        valorsorteado = obterNumero(de, ate);
+        numerosSorteados.push(valorsorteado)
+    }
+
+    alert(numerosSorteados);
+ }
+
+ function obterNumero(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+ }
+
