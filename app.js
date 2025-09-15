f60aff9e956c485c7323707581baca0c317ee5ea
andrey <andreygabrielben@gmail.com>
2025-09-15 19:43:27 -0300

    renomear string de min e max

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function sortear() {
     let QuantidadeASortear = parseInt(document.getElementById("quantidade").value);
-    let min = parseInt(document.getElementById("de").value);
-    let max = parseInt(document.getElementById("ate").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate  = parseInt(document.getElementById("ate").value);
+    let numerosSorteados = obterNumeroAleatorio(de, ate);
     let QuantidadeRestante = QuantidadeASortear;
     if (QuantidadeRestante > 1) {
         QuantidadeRestante--;
