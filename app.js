94d2a963e9146091399155706a1a34ab90a25970
andrey <andreygabrielben@gmail.com>
2025-09-22 19:08:18 -0300

    adicionar função de alteração de status do botão reiniciar

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ function sortear() {
     let ate  = parseInt(document.getElementById("ate").value);
     for (let i=0; i < QuantidadeASortear; i++) {
         obterNumero(de, ate);
-    }
-    exibirResultado(numerosSorteados)
+    };
+    exibirResultado(numerosSorteados);
+    alterarStatusBotão();
  }
 
  function obterNumero(min, max) {
@@ -15,15 +16,25 @@ function sortear() {
     if (numerosSorteados.includes(valorsorteado)) {
         return obterNumero(min, max);
     } else {
-        numerosSorteados.push(valorsorteado)
+        numerosSorteados.push(valorsorteado);
         return valorsorteado;
-    }
+    };
    
  }
 
- 
  function exibirResultado(numerosGerados) {
-    let listaDeNumeros = numerosGerados.join(", ")
-    let resultado = document.getElementById("resultado")
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${listaDeNumeros}.</label>`
- }
+    let listaDeNumeros = numerosGerados.join(", ");
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${listaDeNumeros}.</label>`;
+ };
+
+ function alterarStatusBotão() {
+    let botão = document.getElementById("btn-reiniciar");
+    if (botão.classList.contains("container__botao-desabilitado")) {
+        botão.classList.remove("container__botao-desabilitado");
+        botão.classList.add("container__botao");
+    } else {
+        botão.classList.remove("container__botao");
+        botão.classList.add("container__botao-desabilitado");
+    };
+ };
